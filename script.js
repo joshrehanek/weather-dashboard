533090f26edc35d3edd220ef45ab8e429b27df4d
Josh Rehanek <joshrehanek@gmail.com>
2020-12-18 17:04:59 -0800

    adds addToLocalStorage & city history func,  icon

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 $(document).ready(function () {
+  $(".city-card").hide();
+
+  let history = JSON.parse(window.localStorage.getItem('history')) || [];
+ 
+  for (let i = 0; i < history.length; i++)  {
+
+  cityHistory(history[i]);
+
+  }
 
   $("#find-btn").on("click", function (event) {
     let cityName = $('#search-city').val();
     event.preventDefault()
-    $("#city-form").append();
+    $(".city-card").show();
+  
     currentWeather(cityName);
     fiveDayForecast(cityName);
+    cityHistory(cityName);
+    addToLocalStorage(cityName);
+
 
   })
 })
+
+function addToLocalStorage(cityName){
+  let history = JSON.parse(window.localStorage.getItem('history')) || [];
+  history.push(cityName);
+  window.localStorage.setItem('history', JSON.stringify(history));
+}
+
+function cityHistory(cityName){
+let li = $('<li>').addClass("list-group-item list-group-item-action").text(cityName).attr('value', cityName);
+$('.city-history').append(li);
+
+}
+
 //function to display weather info
 function currentWeather(cityName) {
   console.log('clicked', cityName);
@@ -32,8 +58,8 @@ function currentWeather(cityName) {
     //  variable declarations to extract info from object
     //  build cards with response data
     console.log(response);
-
-    $(".city").html(`<h1> ${response.name} Weather On Demand ${response.weather[0].id}</h1>`);
+    $('#icon').attr('src', `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
+    $(".city").html(`<h1>${response.name}</h1>`);
     $(".temp").text(`Temperature: ${response.main.temp}`)
     $(".wind").text(`Wind Speed: ${response.wind.speed}`);
     $(".humidity").text(`Humidity: ${response.main.humidity}`);
@@ -44,7 +70,7 @@ function currentWeather(cityName) {
 function fiveDayForecast(cityName) {
   console.log('clicked', cityName);
 
-  let forecastURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d`
+  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d`
 
 $.ajax({
   url: forecastURL,
