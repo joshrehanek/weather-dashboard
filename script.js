b7b6a99c2f66d03f4b843ed125afbded44dc7fd3
Josh Rehanek <joshrehanek@gmail.com>
2020-12-18 16:00:10 -0800

    adds city responses classes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-  
-
-  $("#find-btn").on("click", function(event) {
+  $("#find-btn").on("click", function (event) {
     let cityName = $('#search-city').val();
     event.preventDefault()
-
+    $("#city-form").append();
     currentWeather(cityName);
+    fiveDayForecast(cityName);
+
   })
 })
 //function to display weather info
@@ -15,6 +15,7 @@ function currentWeather(cityName) {
 
   // query url
   let queryURL = `https://api.openweathermap.org/data/2.5/weather?appid=74e8a6515f50f51719fd79b020a3284d&q=${cityName}&units=imperial`;
+ 
 
   //function fiveDayForecast (){
   //for loop through cards.length i=5; i < 40 ; i+=8
@@ -23,14 +24,36 @@ function currentWeather(cityName) {
   //take reponse data
 
 
-// AJAX call
+  // AJAX call
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    //  variable declarations to extract info from object
+    //  build cards with response data
+    console.log(response);
+
+    $(".city").html(`<h1> ${response.name} Weather On Demand ${response.weather[0].id}</h1>`);
+    $(".temp").text(`Temperature: ${response.main.temp}`)
+    $(".wind").text(`Wind Speed: ${response.wind.speed}`);
+    $(".humidity").text(`Humidity: ${response.main.humidity}`);
+
+  });
+}
+
+function fiveDayForecast(cityName) {
+  console.log('clicked', cityName);
+
+  let forecastURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d`
+
 $.ajax({
-  url: queryURL,
+  url: forecastURL,
   method: "GET"
 }).then(function (response) {
   //  variable declarations to extract info from object
   //  build cards with response data
   console.log(response);
+
 
 });
 }
