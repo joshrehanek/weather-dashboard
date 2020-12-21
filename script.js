de2d21e1ff74b71f3bbeaa94fb33548f74ede289
Josh Rehanek <joshrehanek@gmail.com>
2020-12-20 18:56:45 -0800

    adds uv index function

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
+
   $(".city-card").hide();
 
   let history = JSON.parse(window.localStorage.getItem('history')) || [];
- 
-  for (let i = 0; i < history.length; i++)  {
 
-  cityHistory(history[i]);
+  for (let i = 0; i < history.length; i++) {
+
+    cityHistory(history[i]);
 
   }
 
@@ -13,25 +14,26 @@ $(document).ready(function () {
     let cityName = $('#search-city').val();
     event.preventDefault()
     $(".city-card").show();
-  
+
     currentWeather(cityName);
     fiveDayForecast(cityName);
     cityHistory(cityName);
     addToLocalStorage(cityName);
+    uvIndex();
 
 
   })
 })
 
-function addToLocalStorage(cityName){
+function addToLocalStorage(cityName) {
   let history = JSON.parse(window.localStorage.getItem('history')) || [];
   history.push(cityName);
   window.localStorage.setItem('history', JSON.stringify(history));
 }
 
-function cityHistory(cityName){
-let li = $('<li>').addClass("list-group-item list-group-item-action").text(cityName).attr('value', cityName);
-$('.city-history').append(li);
+function cityHistory(cityName) {
+  let li = $('<li>').addClass("list-group-item list-group-item-action").text(cityName).attr('value', cityName);
+  $('.city-history').append(li);
 
 }
 
@@ -41,7 +43,7 @@ function currentWeather(cityName) {
 
   // query url
   let queryURL = `https://api.openweathermap.org/data/2.5/weather?appid=74e8a6515f50f51719fd79b020a3284d&q=${cityName}&units=imperial`;
- 
+
 
   //function fiveDayForecast (){
   //for loop through cards.length i=5; i < 40 ; i+=8
@@ -63,23 +65,38 @@ function currentWeather(cityName) {
     $(".temp").text(`Temperature: ${response.main.temp}`)
     $(".wind").text(`Wind Speed: ${response.wind.speed}`);
     $(".humidity").text(`Humidity: ${response.main.humidity}`);
+    //pull lat and lon values for uvIndex function
+    uvIndex(response.coord.lat, response.coord.lon);
+  });
+}
+
+function uvIndex(lat, lon) {
+  var uvURL = "http://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=74e8a6515f50f51719fd79b020a3284d"
+  $.ajax({
+    url: uvURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+      $(".uv-index").text(`UV Index: ${response.value}`)
+
 
   });
 }
+ 
 
 function fiveDayForecast(cityName) {
   console.log('clicked', cityName);
 
   let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d`
 
-$.ajax({
-  url: forecastURL,
-  method: "GET"
-}).then(function (response) {
-  //  variable declarations to extract info from object
-  //  build cards with response data
-  console.log(response);
+  $.ajax({
+    url: forecastURL,
+    method: "GET"
+  }).then(function (response) {
+    //  variable declarations to extract info from object
+    //  build cards with response data
+    console.log(response);
 
 
-});
+  });
 }
