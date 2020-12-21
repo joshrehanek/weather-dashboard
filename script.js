1f20d5610d6f79d2d4ae188be41444f8ec8dde1b
Josh Rehanek <joshrehanek@gmail.com>
2020-12-21 15:31:59 -0800

    adds date and uv index

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 
+
+
   $(".city-card").hide();
+  $(".forecast").hide();
 
   let history = JSON.parse(window.localStorage.getItem('history')) || [];
 
@@ -14,12 +17,12 @@ $(document).ready(function () {
     let cityName = $('#search-city').val();
     event.preventDefault()
     $(".city-card").show();
-
+    $(".forecast").show();
     currentWeather(cityName);
-    fiveDayForecast(cityName);
+    fiveDayForecast();
     cityHistory(cityName);
     addToLocalStorage(cityName);
-    uvIndex();
+    
 
 
   })
@@ -39,63 +42,71 @@ function cityHistory(cityName) {
 
 //function to display weather info
 function currentWeather(cityName) {
-  console.log('clicked', cityName);
-
   // query url
   let queryURL = `https://api.openweathermap.org/data/2.5/weather?appid=74e8a6515f50f51719fd79b020a3284d&q=${cityName}&units=imperial`;
-
-
-  //function fiveDayForecast (){
-  //for loop through cards.length i=5; i < 40 ; i+=8
-  //40 total
-  //get to noon += 8 for one full cycle
-  //take reponse data
-
 
   // AJAX call
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    //  variable declarations to extract info from object
-    //  build cards with response data
+    let date = moment().format('MMMM Do YYYY');
     console.log(response);
     $('#icon').attr('src', `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
-    $(".city").html(`<h1>${response.name}</h1>`);
+    $(".city").html(`<h1>${response.name} ${date}</h1>`);
     $(".temp").text(`Temperature: ${response.main.temp}`)
     $(".wind").text(`Wind Speed: ${response.wind.speed}`);
     $(".humidity").text(`Humidity: ${response.main.humidity}`);
     //pull lat and lon values for uvIndex function
-    uvIndex(response.coord.lat, response.coord.lon);
+    let lat = response.coord.lat
+    console.log(lat);
+    let lon = response.coord.lon
+    console.log(lon);
+    // uvIndex(response.coord.lat, response.coord.lon);
+    var uvURL = "http://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=74e8a6515f50f51719fd79b020a3284d&cnt=1";
+    axios.get(uvURL)
+      .then(function (response) {
+        console.log(response);
+        $(".uv-index").text(`UV Index: ${response.data.value}`)
+
+      });
+
   });
 }
 
-function uvIndex(lat, lon) {
-  var uvURL = "http://api.openweathermap.org/data/2.5/uvi?&lat=" + lat + "&lon=" + lon + "&appid=74e8a6515f50f51719fd79b020a3284d"
-  $.ajax({
-    url: uvURL,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-      $(".uv-index").text(`UV Index: ${response.value}`)
+// function uvIndex(lat, lon) {
+
+//   $.ajax({
+//     url: uvURL,
+//     method: "GET"
+//   }).then(function (response) {
+//     console.log(response);
+//       $(".uv-index").text(`UV Index: ${response.value}`)
 
 
-  });
-}
- 
+//   });
+// }
 
 function fiveDayForecast(cityName) {
   console.log('clicked', cityName);
 
   let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d`
+  console.log(forecastURL);
 
   $.ajax({
     url: forecastURL,
     method: "GET"
   }).then(function (response) {
     //  variable declarations to extract info from object
-    //  build cards with response data
+    let cityID = response.data.id
     console.log(response);
+    console.log(cityID);
+
+    //  build cards with response data
+    //for loop through cards.length i=5; i < 40 ; i+=8
+    //40 total
+    //get to noon += 8 for one full cycle
+    //take reponse data
 
 
   });
