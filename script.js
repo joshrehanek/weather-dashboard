741181294671072dba95db794d31c70409473914
Josh Rehanek <joshrehanek@gmail.com>
2020-12-22 18:26:50 -0800

    adds functional forecast loop

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
 
-
-
   $(".city-card").hide();
   $(".forecast").hide();
+  
 
   let history = JSON.parse(window.localStorage.getItem('history')) || [];
 
@@ -19,13 +18,12 @@ $(document).ready(function () {
     event.preventDefault()
     $(".city-card").show();
     $(".forecast").show();
+    // forecastEl.empty();
     currentWeather(cityName);
     fiveDayForecast(cityName);
     cityHistory(cityName);
     addToLocalStorage(cityName);
     
-
-
   })
 })
 
@@ -86,7 +84,7 @@ function currentWeather(cityName) {
 function fiveDayForecast(cityName) {
   console.log('clicked', cityName);
 
-  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d`
+  let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=74e8a6515f50f51719fd79b020a3284d&units=imperial`
   console.log(forecastURL);
 
   $.ajax({
@@ -94,12 +92,20 @@ function fiveDayForecast(cityName) {
     method: "GET"
   }).then(function (response) {
    console.log(response);
-    //  build cards with response data
-    //for loop through cards.length i=5; i < 40 ; i+=8
-    //40 total
-    //get to noon += 8 for one full cycle
-    //take reponse data
 
+   let forecastTitle = $('<h2>').addClass('mt-3').text('5 Day Forecast');
+   $('#forecast-title').append(forecastTitle);
 
+    for (let i = 4; i < 40; i+=8) {
+      let card = $('<div>').addClass("col md-2 forecast bg-primary text-white ml-2 mb-2 forecast-card");
+      let forecastDate = $('<h5>').text(new Date(response.list[i].dt_txt).toLocaleDateString());
+      let forecastIcon = $("<img>").attr("src", `http://openweathermap.org/img/w/${response.list[i].weather[0].icon}.png`);
+      let forecastTemp = $('<p>').text(`Temp: ${response.list[i].main.temp}`);
+      let forecastHumidity = $('<p>').text(`Humidity: ${response.list[i].main.humidity}`);
+      card.append(forecastDate, forecastIcon, forecastTemp, forecastHumidity);
+      $('#forecast-cards').append(card);
+    }
   });
 }
+
+// array.indexof(i) ===== -1; doesnt exist in local array localStorage
